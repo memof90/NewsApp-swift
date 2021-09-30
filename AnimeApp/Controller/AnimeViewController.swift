b7356a5c1e4662dc8a431f70355bbda80cdc66bf
memo figueredo <memof80@gmail.com>
2021-09-30 17:01:23 -0500

    added interface to animateViewController

---
+++ AnimeApp/Controller/AnimeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AnimeViewController.swift
+//  AnimeApp
+//
+//  Created by Memo Figueredo on 30/9/21.
+//
+
+
+import UIKit
+
+
+class AnimeViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
