a5469c8c6093da416aaebe9041abcd9480dcf173
memo figueredo <memof80@gmail.com>
2021-10-07 17:21:16 -0500

    added search bar

--- AnimeApp/Controller/TopStoriesViewController.swift
+++ AnimeApp/Controller/TopStoriesViewController.swift
@@ -10,20 +10,33 @@ import UIKit
 import Kingfisher
 
 
-class TopStoriesViewController: UIViewController, UISearchBarDelegate {
+class TopStoriesViewController: UIViewController, UISearchBarDelegate{
+
+    
     
 //    MARK: - Variables Global
         var topStories = [TopStoriesViewModel]()
+        var filteredDataTop : [TopStoriesViewModel] = []
+        var filter = false
         var allBooks = [BookListsViewModel]()
+        
     //   MARK: - SearchBar Message to the data not appareance
         fileprivate let searchController = UISearchController(searchResultsController: nil)
         fileprivate let enterSearchTermLabel: UILabel = {
                 let label = UILabel()
-                label.text  = "Please enter your movie Search"
+                label.text  = "Please enter your search"
             label.textAlignment = .center
             label.font = UIFont.boldSystemFont(ofSize: 20)
             return label
         }()
+    
+    fileprivate let notResults: UILabel = {
+        let label = UILabel()
+        label.text = "Not reuslts"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
     
 //    MARK: - IBoutlets
     
@@ -34,20 +47,24 @@ class TopStoriesViewController: UIViewController, UISearchBarDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchResponse()
+//        NetworkServices Fuctions
+        fetchTopStories()
         fetchListBooks()
     
         
-//        TopStoriesCollectionView
+//        TopStoriesCollectionView Protocols
         TopStoriesCollectionView.delegate = self
         TopStoriesCollectionView.dataSource = self
         
-//       BooksCollectionView
+//       BooksCollectionView Protocols
         BooksCollectionView.delegate = self
         BooksCollectionView.dataSource = self
         
+//        Register NibsCells
         registerNibsCells()
-    
+                
+        TopStoriesCollectionView.addSubview(enterSearchTermLabel)
+        enterSearchTermLabel.fillSuperview(padding: .init(top: 100, left: 50, bottom: 0, right: 50))
         
     }
     
@@ -61,12 +78,51 @@ class TopStoriesViewController: UIViewController, UISearchBarDelegate {
             searchController.searchBar.delegate = self
         }
     
+    // MARK: - filter data to search and array
+    
+    var timer: Timer?
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+         print(searchText)
+//        if searchText.isEmpty == false {
+//            topStories.filter({$0.title.contains(searchText)})
+//        }
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+            self.filterContentForSearchText(searchText: searchText)
+    //
+            DispatchQueue.main.async {
+                self.TopStoriesCollectionView.reloadData()
+            }
+           
+        })
+
+    }
+    
+ 
+    
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        if searchText != "" {
+            filteredDataTop = topStories.filter { name in
+                return name.title.lowercased().contains(searchText.lowercased())
+            }
+        } else {
+            self.filteredDataTop = self.topStories
+        }
+    }
+    
+    
+    
+//    MARK: - RegisterNibsCells
     private func registerNibsCells() {
         TopStoriesCollectionView.register(UINib(nibName: TopStoriesCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: TopStoriesCollectionViewCell.identifier)
         BooksCollectionView.register(UINib(nibName: BookListCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: BookListCollectionViewCell.identifier)
     }
-    
-    func fetchResponse() {
+   
+// MARK: - NetWorkServices
+    ///save data to array and save data to viewModel and reload collectionView
+    func fetchTopStories() {
         NetworkServicesModel.shared.fetchTopstories { resp, error in
        
                 self.topStories = resp.map({return TopStoriesViewModel(top: $0)})
@@ -75,7 +131,7 @@ class TopStoriesViewController: UIViewController, UISearchBarDelegate {
                 }
         }
     }
-    
+    ///save data to array and save data to viewModel and reload collectionView
     func fetchListBooks() {
         NetworkServicesModel.shared.fetchListBooks { resp, error in
           
@@ -90,6 +146,9 @@ class TopStoriesViewController: UIViewController, UISearchBarDelegate {
         
     }
     
+        
+
+    
     
     //    MARK: - Changes numberItems of cell depends of section of cell
         /// function to pass number to items depends of cells and
@@ -97,7 +156,8 @@ class TopStoriesViewController: UIViewController, UISearchBarDelegate {
     func collectionViewNumberOfItemsInSection(_ collectionView: UICollectionView) -> Int {
         switch collectionView {
         case TopStoriesCollectionView:
-            return topStories.count
+            enterSearchTermLabel.isHidden = filter ? topStories.count != 0 : filteredDataTop.count != 0
+            return filter ?  topStories.count : filteredDataTop.count
         case BooksCollectionView:
             return allBooks.count
         default:
@@ -106,11 +166,12 @@ class TopStoriesViewController: UIViewController, UISearchBarDelegate {
     }
     
     //    MARK: - Changes NumberOfItemsInSections of cell depends of section of cell
+    /// custom fuctions to pass diferents data to cell and diferents identifier
     func collectionViewcellForItemAt(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         switch collectionView {
         case TopStoriesCollectionView:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TopStoriesCollectionViewCell.identifier, for: indexPath) as! TopStoriesCollectionViewCell
-                cell.setupTopAnimes(top: topStories[indexPath.item])
+            cell.setupTopAnimes(top: filter ?  topStories[indexPath.item] : filteredDataTop[indexPath.item])
             return cell
         case BooksCollectionView:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BookListCollectionViewCell.identifier, for: indexPath) as! BookListCollectionViewCell
@@ -124,10 +185,13 @@ class TopStoriesViewController: UIViewController, UISearchBarDelegate {
 //    MARK: - IBAction
     
     @IBAction func searchActionBtn(_ sender: Any) {
-        setupSearchBar()
+        
+            setupSearchBar()
     }
     
 }
+
+// MARK: - Extension to TopStories to pass Protocols numberOfItemsInSection, cellForItemAt
 
 extension TopStoriesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -137,12 +201,6 @@ extension TopStoriesViewController: UICollectionViewDelegate, UICollectionViewDa
  
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TopStoriesCollectionViewCell.identifier, for: indexPath) as! TopStoriesCollectionViewCell
-//
-//
-//        cell.setupTopAnimes(top: topStories[indexPath.item])
-//
-//        return cell
         
         return collectionViewcellForItemAt(collectionView, cellForItemAt: indexPath)
     }
